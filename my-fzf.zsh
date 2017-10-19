5a795de70e5695f3fc9129b3611f59f23258e335
Marius Matei <mariusmatei31@gmail.com>
2017-10-20 02:09:20 +0300

    improve the fzf git preview commands

--- my-fzf.zsh
+++ my-fzf.zsh
@@ -26,86 +26,13 @@ export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 # - https://junegunn.kr/2016/07/fzf-git/
 # - https://github.com/junegunn/fzf/wiki/examples#git
 # Will return non-zero status if the current directory is not managed by git
+# GIT heart FZF
+#
 is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
-
-# fshow - git commit browser
-fshow() {
-  git log --graph --color=always \
-      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
-  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
-      --bind "ctrl-m:execute:
-                (grep -o '[a-f0-9]\{7\}' | head -1 |
-                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
-                {}
-FZF-EOF"
-  zle redisplay
-}
-zle     -N    fzf-locate-widget
-
-# fgsh - get git commit sha
-# example usage: git rebase -i `fcs`
-fgsh() {
-  local commits commit
-  commits=$(git log --color=always --pretty=oneline --abbrev-commit --reverse) &&
-  commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
-  echo -n $(echo "$commit" | sed "s/ .*//")
-}
-
-# fstash - easier way to deal with stashes
-# type fstash to get a list of your stashes
-# enter shows you the contents of the stash
-# ctrl-d shows a diff of the stash against your current HEAD
-# ctrl-b checks the stash out as a branch, for easier merging
-fgst() {
-  local out q k sha
-  while out=$(
-    git stash list --pretty="%C(yellow)%h %>(14)%Cgreen%cr %C(blue)%gs" |
-    fzf --ansi --no-sort --query="$q" --print-query \
-        --expect=ctrl-d,ctrl-b);
-  do
-    mapfile -t out <<< "$out"
-    q="${out[0]}"
-    k="${out[1]}"
-    sha="${out[-1]}"
-    sha="${sha%% *}"
-    [[ -z "$sha" ]] && continue
-    if [[ "$k" == 'ctrl-d' ]]; then
-      git diff $sha
-    elif [[ "$k" == 'ctrl-b' ]]; then
-      git stash branch "stash-$sha" $sha
-      break;
-    else
-      git stash show -p $sha
-    fi
-  done
-}
-
-
-
-
-# fco - checkout git branch/tag
-fco() {
-  local tags branches target
-  tags=$(
-    git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
-  branches=$(
-    git branch --all | grep -v HEAD             |
-    sed "s/.* //"    | sed "s#remotes/[^/]*/##" |
-    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
-  target=$(
-    (echo "$tags"; echo "$branches") |
-    fzf-tmux -l30 -- --no-hscroll --ansi +m -d "\t" -n 2) || return
-  git checkout $(echo "$target" | awk '{print $2}')
-}
-
-# GIT heart FZF
 # -------------
 
-is_in_git_repo() {
-  git rev-parse HEAD > /dev/null 2>&1 || echo "This_is_not_a_git_repository"
-}
 
 fzf-down() {
   fzf --height 90% "$@" --border
@@ -140,10 +67,10 @@ function "${FZF_PREFIX}gt" () {
 
 function "${FZF_PREFIX}gh" () {
   is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cr  %C(blue)%cn %C(auto)%h %s" --graph --color=always |
+  git log --date=short --format="%C(green)%C(bold)%cd %C(blue)%an %C(auto)%h %s" --graph --color=always |
   fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
     --header 'Press CTRL-S to toggle sort' \
-    --preview-window right:30% \
+    --preview-window right:40% \
     --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
   grep -o "[a-f0-9]\{7,\}"
 }
@@ -171,6 +98,7 @@ bind-git-helper() {
     eval "bindkey '^g^$c' fzf-g$c-widget"
   done
 }
+
 bind-git-helper f b t r h
 unset -f bind-git-helper
 ###################END OF GIT GOODIES###################
