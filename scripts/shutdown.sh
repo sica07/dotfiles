aed2e6fef91cdce528320d87510d4cc86dcc5693
Marius Matei <mariusmatei31@gmail.com>
2022-01-12 19:37:36 +0200

    use systemctl for power management

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-ACTION=$(echo -e "logout\nreboot\nshutdown\ncancel" | rofi -dmenu -p "Shut down computer?") 
+ACTION=$(echo -e "logout\nsuspend\nhibernate\nreboot\nshutdown\ncancel" | rofi -dmenu -p "Shut down computer?")
 if [ $ACTION == "logout" ]; then
      i3-msg exit
 elif [ $ACTION == "reboot" ]; then
-    gksudo "reboot"
-elif [ $ACTION == "shutdown" ]; then 
-    gksudo "shutdown -h now"
+    #gksudo "reboot"
+    systemctl reboot
+elif [ $ACTION == "suspend" ]; then
+    systemctl suspend
+elif [ $ACTION == "hibernate" ]; then
+    systemctl suspend-then-hibernate
+elif [ $ACTION == "shutdown" ]; then
+    #gksudo "shutdown -h now"
+    systemctl poweroff
 fi
 
