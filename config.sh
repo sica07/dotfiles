3a313e78c0d1bb3d89b697bfd754bb57de1c99e3
Marius Matei <mariusmatei31@gmail.com>
2022-01-25 19:21:59 +0200

    replace surf with luakit

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ echo """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common zsh locate ripgrep fzf autoconf ssh-askpass redshift ncal &&
 sudo apt install -y jq htop tmux compton python python3 python3-dev python3-pip python3-setuptools sysstat git-extras dict editorconfig diodon exa &&
-sudo apt install -y tig html-xml-utils duf python3-neovim meld zathura surf mpv fd-find alacritty newsboat network-manager-openvpn-gnome guake tint2
+sudo apt install -y tig html-xml-utils duf python3-neovim meld zathura surf mpv fd-find alacritty newsboat network-manager-openvpn-gnome guake tint2 luakit
 
 sudo flatpak install -y postman slack dbeaver
 #install nodejs
